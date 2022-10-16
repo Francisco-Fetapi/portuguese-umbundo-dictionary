7821693aa331b6e76588da55f4207154deb44ffa
Francisco Fetapi <franciscofetapi10@gmail.com>
2022-10-16 07:12:07 +0100

    feat: dark theme provider created

--- src/components/ModalMenu.tsx
+++ src/components/ModalMenu.tsx
@@ -7,8 +7,6 @@ import DialogTitle from "@mui/material/DialogTitle";
 import Slide from "@mui/material/Slide";
 import { TransitionProps } from "@mui/material/transitions";
 import { Text } from "../styles/General";
-
-import { createTheme, ThemeProvider } from "@mui/material";
 import MenuList from "./MenuList";
 
 const Transition = React.forwardRef(function Transition(
@@ -26,50 +24,42 @@ interface Props {
 }
 
 export default function ModalMenu({ handleClose, open }: Props) {
-  const theme = createTheme({
-    palette: {
-      mode: "dark",
-    },
-  });
-
   return (
-    <ThemeProvider theme={theme}>
-      <Dialog
-        open={open}
-        TransitionComponent={Transition}
-        keepMounted
-        maxWidth="md"
-        PaperProps={{
-          sx: {
-            height: "100%",
-            width: 500,
-            background: "var(--primary-color2)",
-            color: "white",
-            borderRadius: "20px",
-            position: "relative",
-            margin: "5px",
-            maxWidth: "var(--max-width)",
-          },
+    <Dialog
+      open={open}
+      TransitionComponent={Transition}
+      keepMounted
+      maxWidth="md"
+      PaperProps={{
+        sx: {
+          height: "100%",
+          width: 500,
+          background: "var(--primary-color2)",
+          color: "white",
+          borderRadius: "20px",
+          position: "relative",
+          margin: "5px",
+          maxWidth: "var(--max-width)",
+        },
+      }}
+    >
+      <DialogTitle
+        variant="h5"
+        align="center"
+        sx={{
+          color: "#D0D0D0",
         }}
       >
-        <DialogTitle
-          variant="h5"
-          align="center"
-          sx={{
-            color: "#D0D0D0",
-          }}
-        >
-          Menu
-        </DialogTitle>
-        <DialogContent sx={{ padding: 0 }}>
-          <Text variant="subtitle2">
-            <MenuList />
-          </Text>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Fechar</Button>
-        </DialogActions>
-      </Dialog>
-    </ThemeProvider>
+        Menu
+      </DialogTitle>
+      <DialogContent sx={{ padding: 0 }}>
+        <Text variant="subtitle2">
+          <MenuList />
+        </Text>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Fechar</Button>
+      </DialogActions>
+    </Dialog>
   );
 }
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,5 +1,70 @@
+import {
+  Avatar,
+  Box,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+} from "@mui/material";
+import React from "react";
+import { FaHistory, FaSearch } from "react-icons/fa";
+import { MdHistory, MdLanguage, MdPalette, MdSearch } from "react-icons/md";
 import PageHeader from "../components/PageHeader";
 
+interface IOption {
+  label: string;
+  icon: React.ReactNode;
+  textSecondary: string;
+  secondaryComponent?: React.ReactNode;
+}
+
 export default function Settings() {
-  return <PageHeader pageName="Definições">Settings</PageHeader>;
+  const options: IOption[] = [
+    {
+      label: "Pesquisa Automática",
+      icon: <MdSearch />,
+      textSecondary: `Com essa opção selecionada, os resultados da pesquisa surgem enquanto você digita o texto, caso contrário, terás que clicar no botão pesquisar.`,
+    },
+    {
+      label: "Armazenas histórico",
+      icon: <MdHistory />,
+      textSecondary: `Sempre salvar o historico de pesquisa de palavras`,
+    },
+    {
+      label: "Tema",
+      icon: <MdPalette />,
+      textSecondary: `Altere o tema da aplicaçã entre claro e escuro`,
+    },
+    {
+      label: "Idioma da aplicação",
+      icon: <MdLanguage />,
+      textSecondary: `Alterar o idioma da aplicação`,
+    },
+  ];
+
+  return (
+    <PageHeader pageName="Definições">
+      <Box>
+        <List
+          sx={{
+            width: "100%",
+            padding: 0,
+            zoom: "0.9",
+          }}
+        >
+          {options.map((option) => (
+            <ListItem button divider key={option.label}>
+              <ListItemAvatar>
+                <Avatar>{option.icon}</Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary={option.label}
+                secondary={option.textSecondary}
+              />
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </PageHeader>
+  );
 }
