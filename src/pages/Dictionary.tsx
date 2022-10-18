e2f98aaea453216b5c3ad795784a19c29cd818f0
Francisco Fetapi <franciscofetapi10@gmail.com>
2022-10-18 10:01:13 +0100

    refactor: Dictionary

--- src/pages/Dictionary.tsx
+++ src/pages/Dictionary.tsx
@@ -1,5 +1,101 @@
+import { useInputState } from "@mantine/hooks";
+import { Autocomplete, Box, Grid, TextField } from "@mui/material";
+import { useState } from "react";
 import PageHeader from "../components/PageHeader";
+import { IWordClasses } from "../database/IWordClasses";
+import wordClasses from "../database/wordClasses.json";
+
+interface IOption {
+  value: string;
+  label: string;
+}
+
+const options1: IOption[] = [
+  {
+    label: "Todos",
+    value: "all",
+  },
+];
+Object.keys(wordClasses).forEach((word) => {
+  options1.push({
+    value: word,
+    label: wordClasses[word as keyof IWordClasses],
+  });
+});
+
+const options2: IOption[] = [
+  { label: "Todos", value: "" },
+  { label: "Sem exemplos", value: "withoutExample" },
+  { label: "Com exemplos", value: "withExample" },
+];
 
 export default function Dictionary() {
-  return <PageHeader pageName="Dicionário">Verbs</PageHeader>;
+  const [classFilter, setClassFilter] = useState<IOption>(options1[0]);
+  const [exampleFilter, setExampleFilter] = useState<IOption>(options2[0]);
+  const [search, handleSearch] = useInputState("");
+
+  return (
+    <PageHeader pageName="Dicionário">
+      <Box px={1} pt={1}>
+        <Grid container gap={1.5}>
+          <Grid item xs>
+            <Autocomplete
+              disablePortal
+              id="combo-box-demo"
+              options={options1}
+              onChange={(_, newValue) => {
+                if (!newValue) return;
+                if (options1.includes(newValue)) {
+                  setClassFilter(newValue);
+                }
+              }}
+              defaultValue={classFilter}
+              sx={{ width: "100%" }}
+              getOptionLabel={(option) => option.label}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Filtrar por classe"
+                  size="small"
+                />
+              )}
+            />
+          </Grid>
+          <Grid item xs>
+            <Autocomplete
+              disablePortal
+              options={options2}
+              onChange={(_, newValue) => {
+                if (!newValue) return;
+                if (options2.includes(newValue)) {
+                  setExampleFilter(newValue);
+                }
+              }}
+              defaultValue={exampleFilter}
+              sx={{ width: "100%" }}
+              getOptionLabel={(option) => option.label}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Filtrar por exemplos"
+                  size="small"
+                />
+              )}
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="Termo de pesquisa"
+              size="small"
+              variant="outlined"
+              value={search}
+              onChange={handleSearch}
+            />
+          </Grid>
+        </Grid>
+      </Box>
+    </PageHeader>
+  );
 }
