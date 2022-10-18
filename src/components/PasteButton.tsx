eb1ef9615ff3557d457c62971f26eca310201850
Francisco Fetapi <franciscofetapi10@gmail.com>
2022-10-18 15:26:32 +0100

    feat: paste button action defined

--- src/components/PasteButton.tsx
+++ src/components/PasteButton.tsx
@@ -1,20 +1,49 @@
+import { useWindowEvent } from "@mantine/hooks";
 import { Button } from "@mui/material";
+import { useEffect, useState } from "react";
 import { MdContentPaste } from "react-icons/md";
 
 export default function PasteButton() {
+  const [textToPaste, setTextToPaste] = useState("");
+  async function setCopied() {
+    try {
+      let copied = await navigator.clipboard.readText();
+      setTextToPaste(copied);
+    } catch (e) {
+      console.log("erro", e);
+    }
+  }
+
+  useEffect(() => {
+    setCopied();
+  }, []);
+
+  useWindowEvent("focus", (event) => {
+    if (textToPaste.length === 0) {
+      setCopied();
+    }
+  });
+
+  function handle() {
+    console.log(textToPaste);
+  }
   return (
     <div>
-      <Button
-        startIcon={<MdContentPaste />}
-        sx={(theme) => ({
-          background: theme.palette.primary.main,
-        })}
-        variant="contained"
-        disableElevation
-        style={{ borderRadius: "18px" }}
-      >
-        Colar
-      </Button>
+      {textToPaste.length > 0 && (
+        <Button
+          startIcon={<MdContentPaste />}
+          sx={(theme) => ({
+            background: theme.palette.primary.main,
+          })}
+          variant="contained"
+          disableElevation
+          size="small"
+          style={{ borderRadius: "18px" }}
+          onClick={handle}
+        >
+          Colar&nbsp;&nbsp;&nbsp;
+        </Button>
+      )}
     </div>
   );
 }
