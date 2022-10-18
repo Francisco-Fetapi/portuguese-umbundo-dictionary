bf29f738f73856d4c5e6ddd1fc5aec751e233528
Francisco Fetapi <franciscofetapi10@gmail.com>
2022-10-18 16:00:48 +0100

    feat: config automatic search defined

--- src/components/InputText.tsx
+++ src/components/InputText.tsx
@@ -1,27 +1,46 @@
-import { useTheme } from "@mui/material";
+import { useDebouncedValue, useInputState } from "@mantine/hooks";
+import { Box, useTheme } from "@mui/material";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { selectTextToTranslate } from "../store/App.selectors";
+import { selectSettings } from "../store/App.selectors";
 import { setTextToTranslate } from "../store/App.store";
 import { InputArea } from "../styles/General";
 
-type FuncHandleChange = React.ChangeEventHandler<HTMLInputElement> | undefined;
+type FuncFormEventHandler = React.FormEventHandler<HTMLFormElement> | undefined;
 
 export default function InputText() {
-  const value = useSelector(selectTextToTranslate);
+  const [value, handleChange] = useInputState("");
+  const [debounced] = useDebouncedValue(value, 1000);
+  const { automaticSearch } = useSelector(selectSettings);
   const dispatch = useDispatch();
-  const handleChange: FuncHandleChange = (e) => {
-    dispatch(setTextToTranslate(e.target.value));
-  };
   const theme = useTheme();
+
+  useEffect(() => {
+    if (automaticSearch) {
+      dispatch(setTextToTranslate(value));
+    }
+  }, [debounced]);
+
+  const handleSearch: FuncFormEventHandler = (e) => {
+    e.preventDefault();
+    dispatch(setTextToTranslate(value));
+  };
 
   return (
     <InputArea>
-      <input
-        value={value}
-        onChange={handleChange}
-        placeholder="Introduza a palavra"
-        className={theme.palette.mode}
-      />
+      <Box
+        component="form"
+        noValidate
+        autoComplete="off"
+        onSubmit={handleSearch}
+      >
+        <input
+          value={value}
+          onChange={handleChange}
+          placeholder="Introduza a palavra"
+          className={theme.palette.mode}
+        />
+      </Box>
     </InputArea>
   );
 }
